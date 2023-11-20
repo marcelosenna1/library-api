0c115202bc7573dd3a373fedcf6152df8cc995c6
marcelosenna1 <marcelomarcos2@gmail.com>
2023-11-20 16:31:15 -0300

    Exception Handler para teste passar

--- src/main/java/com/sena/libraryapi/api/resource/BookController.java
+++ src/main/java/com/sena/libraryapi/api/resource/BookController.java
@@ -1,14 +1,19 @@
 package com.sena.libraryapi.api.resource;
 
 import com.sena.libraryapi.api.dto.BookDTO;
+import com.sena.libraryapi.api.exception.ApiErrors;
 import com.sena.libraryapi.api.model.Book;
 import com.sena.libraryapi.api.service.BookService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
@@ -26,5 +31,12 @@ public class BookController {
         Book entity = modelMapper.map(dto, Book.class);
         entity = service.save(entity);
         return modelMapper.map(entity, BookDTO.class);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleValidationExceptions(MethodArgumentNotValidException ex){
+        BindingResult bindingResult = ex.getBindingResult();
+        return new ApiErrors (bindingResult);
     }
 }
