ace2413e1a253640c85830088226683f679d622f
marcelosenna1 <marcelomarcos2@gmail.com>
2023-11-27 01:41:31 -0300

    [test] - implementação de teste de integração do método save

--- src/test/java/com/sena/libraryapi/model/repository/BookRepositoryTest.java
+++ src/test/java/com/sena/libraryapi/model/repository/BookRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.sena.libraryapi.model.repository;
 
 import com.sena.libraryapi.model.entity.Book;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +13,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -28,7 +29,7 @@ public class BookRepositoryTest {
 
     @Test
     @DisplayName("Deve retornar verdadeiro quando existir um livro na base com o isbn informado")
-    void returnTrueWhenIsbnExists(){
+    void returnTrueWhenIsbnExists() {
 
         String isbn = "123";
         Book book = createNewBookIsbn(isbn);
@@ -41,7 +42,7 @@ public class BookRepositoryTest {
 
     @Test
     @DisplayName("Deve retornar false quando não existir um livro na base com o isbn informado")
-    void returnFalseWhenIsbnExists(){
+    void returnFalseWhenIsbnExists() {
 
         String isbn = "123";
 
@@ -52,7 +53,7 @@ public class BookRepositoryTest {
 
     @Test
     @DisplayName("Deve obter um livro por ID")
-    void findByIdTest(){
+    void findByIdTest() {
 
         Book book = createNewBookIsbn("123");
 
@@ -62,6 +63,34 @@ public class BookRepositoryTest {
 
         assertThat(foundBook.isPresent()).isTrue();
 
+    }
+
+    @Test
+    @DisplayName("Deve salvar um livro")
+    void saveBookTest() {
+        Book book = createNewBookIsbn("123");
+
+        Optional<Book> savedBook = Optional.of(repository.save(book));
+
+        assertNotNull(savedBook.get());
+        assertNotNull(savedBook.get().getId());
+        assertNotNull(savedBook.get().getAuthor());
+        assertNotNull(savedBook.get().getTitle());
+        assertNotNull(savedBook.get().getIsbn());
+    }
+
+    @Test
+    @DisplayName("Deve deletar um livro")
+    void deleteBookTest() {
+        Book book = createNewBookIsbn("123");
+        entityManager.persist(book);
+
+        Book foundBook = entityManager.find(Book.class, book.getId());
+        repository.delete(foundBook);
+
+        Book deletedBook = entityManager.find(Book.class, book.getId());
+
+        assertNull(deletedBook);
     }
 
     private static Book createNewBookIsbn(String isbn) {
